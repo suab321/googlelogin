0cf9fa64f14ef2aaa44712b711e59ecff440e24c
Abhinav Singh <adityasaha890@gmail.com>
2019-01-19 16:54:43 +0530

    Df

--- oauth-google/route.js
+++ oauth-google/route.js
@@ -10,7 +10,8 @@ router.get('/login',
     })
 )
 router.get('/google_redirect',passport.authenticate('google',{failureRedirect:'http://localhost:4200/login/student'}),(req,res)=>{
-    res.redirect("http://localhost:4200/dashboard/university");
+    console.log(req.user);
+    res.redirect(`http://localhost:4200/googlelogin/${req.user.emails[0].value}`);
 })
 
 module.exports={router};
